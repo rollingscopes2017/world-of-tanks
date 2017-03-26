81accd5d821a4cf93316ad899eefaf5f1bbd6b39
qwertyniop1 <svitas1997@gmail.com>
2017-03-26 13:25:16 +0300

    simple-game/feature: dynamic entities, collisions detection

--- js/main.js
+++ js/main.js
@@ -1,34 +1,93 @@
-const Entity = function(width, height, color, x, y) {
+const Entity = function(width, height, x, y, color) {
     this.width = width;
     this.height = height;
     this.color = color;
     this.x = x;
     this.y = y;
+}
+
+
+const DynamicEntity = function(width, height, x, y, color) {
+    Entity.call(this, width, height, x, y, color);
     this.speed = {
         x: 0,
         y: 0
     };
 }
 
-Entity.prototype.update = function() {
+DynamicEntity.prototype = Object.create(Entity.prototype);
+DynamicEntity.prototype.constructor = DynamicEntity;
+
+DynamicEntity.prototype.update = function() {
     this.x += this.speed.x;
     this.y += this.speed.y;
 };
 
-Entity.prototype.control = function(directions) {
-    this.speed.x = this.speed.y = 0;
-    directions.forEach(direction => {
-        if (direction === 'UP') {
-            this.speed.y = -5;
-        } else if (direction === 'DOWN') {
-            this.speed.y = 5;
-        } else if (direction === 'LEFT') {
-            this.speed.x = -5;
-        } else if (direction === 'RIGHT') {
-            this.speed.x = 5;
+DynamicEntity.prototype.collide = function(object) {
+    if (this.speed.x !== 0) {
+        if (this.x < object.x) {
+            this.x = object.x - this.width;
+        } else {
+            this.x = object.x + object.width;
         }
-    });
+    }
+    if (this.speed.y !== 0) {
+        if (this.y < object.y) {
+            this.y = object.y - this.height;
+        } else {
+            this.y = object.y + object.height;
+        }
+    }
 };
+
+DynamicEntity.prototype.control = function(direction) {
+    this.speed.x = this.speed.y = 0;
+    if (direction === 'UP') {
+        this.speed.y = -5;
+    } else if (direction === 'DOWN') {
+        this.speed.y = 5;
+    } else if (direction === 'LEFT') {
+        this.speed.x = -5;
+    } else if (direction === 'RIGHT') {
+        this.speed.x = 5;
+    }
+};
+
+
+const World = {
+    entities: [],
+    player: null,
+    init: function() {
+        this.entities = [
+            new DynamicEntity(50, 50, 200, 200, 'red'),
+            new DynamicEntity(50, 50, 0, 200, 'red'),
+            new Entity(50, 50, 80, 10, 'green'),
+            new Entity(50, 50, 130, 10, 'green'),
+            new Entity(50, 50, 180, 10, 'green'),
+            new Entity(50, 50, 230, 10, 'green'),
+            new Entity(50, 50, 280, 10, 'green'),
+            new Entity(50, 50, 10, 50, 'blue')
+        ]
+        this.player = this.entities[0];
+    },
+    step: function(input) {
+        this.player.control(input.pop());
+        this.entities.filter(entity => entity instanceof DynamicEntity).forEach(entity => entity.update());
+        this.checkCollision();
+    },
+    checkCollision: function() {
+        this.entities.filter(entity => entity instanceof DynamicEntity).forEach(entity => this.entities.forEach(e => {
+            if (entity !== e) {
+                if (entity.x < e.x + e.width &&
+                entity.x + entity.width > e.x &&
+                entity.y < e.y + e.height &&
+                entity.height + entity.y > e.y) {
+                    entity.collide(e);
+                }
+            }
+        }));
+    }
+}
 
 
 const Renderer = {
@@ -88,24 +147,16 @@ const Game = {
     },
     input: InputProcessor,
     renderer: Renderer,
-    entities: [],
-    player: null,
+    world: World,
     start: function() {
         this.input.init(window);
         this.renderer.init();
-        this.interval = setInterval(this.update, this.config.frameRate);
-
-        this.entities = [
-            new Entity(50, 50, 'red', 10, 10),
-            new Entity(50, 50, 'green', 80, 10),
-            new Entity(50, 50, 'blue', 10, 50)
-        ]
-        this.player = this.entities[0];
+        this.interval = setInterval(this.update.bind(this), this.config.frameRate);
+        this.world.init();
     },
     update: function() {
-        Game.player.control(Game.input.process());
-        Game.entities.forEach(entity => entity.update());
-        Game.renderer.render(Game.entities);
+        this.world.step(this.input.process());
+        this.renderer.render(this.world.entities);
     }
 }
 
